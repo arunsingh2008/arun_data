f7fba04b68298c293eafd3e13dd231e1bc21aecf
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-08-30 21:18:30 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,19 @@
+# import numpy as np
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import make_pipeline
+
+# X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+# Y = np.array([1, 1, 2, 2])
+# # Always scale the input. The most convenient way is to use a pipeline.
+# clf = make_pipeline(StandardScaler(),SGDClassifier(max_iter=10, tol=1e-3))
+# # print(type(clf))
+# clf.fit(X, Y)
+# print(clf.predict([[-0.8, -1]]))
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.svm import OneClassSVM
+from sklearn.linear_model import SGDOneClassSVM
+from sklearn.kernel_approximation import Nystroem
+from sklearn.pipeline import make_pipeline
