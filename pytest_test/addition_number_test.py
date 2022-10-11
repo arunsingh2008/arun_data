d7f19aa12fb2f87786e45d661d576b9b48a7eb96
sachin singh <sachin2314@gmail.com>
2022-10-11 14:08:20 +0100

    Failing the build

--- pytest_test/addition_number_test.py
+++ pytest_test/addition_number_test.py
@@ -2,7 +2,7 @@ import pytest
 from addition_number import addNum
 
 def test_addition_negative():
-    assert addNum(3,4) != 0
+    assert addNum(3,4) == 0
     
 def test_addition_postive():
     assert addNum(3,4) == 7
