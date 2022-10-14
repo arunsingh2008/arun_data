f371d1a00bf7c440f95b98147209bb253220d47f
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-10-14 17:51:30 +0530

    Update script_bash.sh

--- script_bash.sh
+++ script_bash.sh
@@ -1,7 +1,9 @@
 echo "bash script is starting at $(date) "
 ls
 pwd
-python3 -m pytest pytest_test/addition_number_test.py
+python3 -m pytest pytest_test/addition_number_test.py || [[ $? -eq 1 ]]
 echo "This is for auto update in github"
+
+
 
 
