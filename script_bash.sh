fdf26df08ce53605b1ce9fec288e7d3535ae1e51
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-10-14 18:33:39 +0530

    Update script_bash.sh

--- script_bash.sh
+++ script_bash.sh
@@ -4,10 +4,10 @@ pwd
 # python3 -m pytest pytest_test/addition_number_test.py || [[ $? -eq 1 ]]
 # python3 -m pytest pytest_test/addition_number_test.py --suppress-tests-failed-exit-code
 python3 -m pytest pytest_test/addition_number_test.py
-# status=${PIPESTATUS[0]} # status of run_tests.py
-if [ $? -ne 0 ]; then
+status=$? # status of run_tests.py
+if [ $status -ne 0 ]; then
     echo 'ERROR: pytest failed, exiting ...'
-    exit 1
+    exit $status
 fi
 echo "This is for auto update in github"
 
