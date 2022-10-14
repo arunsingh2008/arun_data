be247abdb2054259a4ce4090ca2d3dc1f60da42a
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-10-14 17:54:33 +0530

    Update script_bash.sh

--- script_bash.sh
+++ script_bash.sh
@@ -1,7 +1,8 @@
 echo "bash script is starting at $(date) "
 ls
 pwd
-python3 -m pytest pytest_test/addition_number_test.py || [[ $? -eq 1 ]]
+# python3 -m pytest pytest_test/addition_number_test.py || [[ $? -eq 1 ]]
+python3 -m pytest pytest_test/addition_number_test.py --suppress-tests-failed-exit-code
 echo "This is for auto update in github"
 
 
