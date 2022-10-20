aa39e5dde60def96e2bcd309e480e16709c0469b
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-10-20 20:23:14 +0530

    Update script_bash.sh

--- script_bash.sh
+++ script_bash.sh
@@ -3,7 +3,8 @@ ls
 pwd
 # python3 -m pytest pytest_test/addition_number_test.py || [[ $? -eq 1 ]]
 # python3 -m pytest pytest_test/addition_number_test.py --suppress-tests-failed-exit-code
-python3 -m pytest pytest_test/addition_number_test.py
+# python3 -m pytest pytest_test/addition_number_test.py
+python3 -m pytest --cov=addNum --junitxml=./xmlReport/output.xml
 status=$? # status of run_tests.py
 if [ $status -ne 0 ]; then
     echo 'ERROR: pytest failed, exiting ...'
