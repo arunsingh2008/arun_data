0e3ed9281011fa6f11d7c10fdfbb16228a39b102
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2022-10-25 18:11:28 +0530

    Update addition_number_test.py

--- pytest_test/addition_number_test.py
+++ pytest_test/addition_number_test.py
@@ -2,8 +2,8 @@ import pytest
 from addition_number import addNum
 
 def test_addition_negative():
-    assert addNum(3,4) == 0
+    assert addNum(3,4) != 0
     
-def test_addition_postive():
-    assert addNum(3,4) == 7
+# def test_addition_postive():
+#     assert addNum(3,4) == 7
 
